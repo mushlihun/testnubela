d1183c9009c0012da7ffb2db52a171c3bae2950c
MUSHLIHUN <mushlihun@asiapay.com>
2023-06-07 12:00:09 +0700

    update

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const express= require('express')
+var mustacheExpress = require('mustache-express')
 const app = express()
 
 const port = 3000;
-
+var insults = ['{"id":42,"method":"echo","params":{"message":"Hello"}}'];
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
-
+app.engine('html', mustacheExpress());
+app.set('view engine', 'html');
+app.set('views', __dirname);
 app.use(express.json());
+app.get('/', function (req, res) {
+  res.render('echo', {});
+});
+
 app.get('/foo', function (req, res) {
     res.json({"foo": "bar"});
 });
-
-// app.use(express.urlencoded({
-//   extended: true
-// }));
-
+/*
+app.use(express.urlencoded({
+  extended: true
+}));
+*/
 app.post('/bar', function(req, res) {
   var bodys = req.body;
   var id = bodys.id;
